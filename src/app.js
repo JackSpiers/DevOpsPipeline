6ac38172606ced288007f0d20c2c49d4a38dcd69
Jack Spiers <s223929662@deakin.edu.au>
2025-06-02 07:35:37 +1000

    Update app.js
    adjustmnets

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); //IMPORTANT: enable JSON parsing for POST/PUT
 
 app.get('/', (req, res) => {
   res.send('Task Manager running.');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
