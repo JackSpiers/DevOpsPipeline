a7d16b1e7809e5cdade83b12ac4884d04b6d7ae4
Jack Spiers <s223929662@deakin.edu.au>
2025-06-02 21:42:45 +1000

    Update start.js

--- src/start.js
+++ src/start.js
@@ -1,6 +1,5 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on 0.0.0.0:${PORT}`);
 });
